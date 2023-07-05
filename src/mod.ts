bb88211c3ee0f3d460dc39976bad6b874f474293
Rogerio Munhoz <roz@rjmunhoz.me>
2023-07-04 23:45:32 -0300

    wip: new API proposal

--- src/mod.ts
+++ src/mod.ts
@@ -11,85 +11,15 @@ import {
 } from "./deps.deno.ts";
 
 type MaybeArray<T> = T | T[];
+type SetMyCommandsParams = {
+  scope?: BotCommandScope;
+  language_code?: string;
+  commands: BotCommand[];
+};
 
-export class CommandScope<C extends Context> extends Composer<C> {
-  #scope: BotCommandScope = { type: "default" };
-  #commands: Command<C>[] = [];
-
-  private constructor(scope: BotCommandScope) {
-    super();
-    this.#scope = scope;
-  }
-
-  public static default() {
-    return new CommandScope({ type: "default" });
-  }
-
-  public static private() {
-    return new CommandScope({ type: "all_private_chats" });
-  }
-
-  public static groups() {
-    return new CommandScope({ type: "all_group_chats" });
-  }
-
-  public static admins() {
-    return new CommandScope({ type: "all_chat_administrators" });
-  }
-
-  public static chat(chatId: number | string) {
-    return new CommandScope({ type: "chat", chat_id: chatId });
-  }
-
-  public static chatAdmins(chatId: number | string) {
-    return new CommandScope({ type: "chat_administrators", chat_id: chatId });
-  }
-
-  public static chatMember(chatId: number | string, userId: number) {
-    return new CommandScope({ type: "chat_member", chat_id: chatId, user_id: userId });
-  }
-
-  public add(commands: MaybeArray<Command<C>>) {
-    const commandArr = Array.isArray(commands) ? commands : [commands];
-    this.#commands.push(...commandArr);
-    this.use(...commandArr);
-    return this;
-  }
-
-  private get languages() {
-    return Array.from<string | undefined>(this.#commands.reduce((langs, command) => {
-      langs;
-      Array.from(command.localizedProperties.keys()).forEach((lang) => langs.add(lang));
-      return langs;
-    }, new Set<string>())).concat([undefined]);
-  }
-
-  public toParams(): Array<{ scope: BotCommandScope; language_code?: string; commands: BotCommand[] }> {
-    return this.languages.map((languageCode) => ({
-      scope: this.#scope,
-      language_code: languageCode,
-      commands: this.#commands.map((command) => command.toObject(languageCode)),
-    }));
-  }
-
-  public setFor(bot: Bot<C>) {
-    const language = this.toParams();
-    return Promise.all(language.map((args) => bot.api.raw.setMyCommands(args)));
-  }
-
-  public deleteFor(bot: Bot<C>) {
-    return Promise.all(
-      this.languages.map((languageCode) =>
-        bot.api.deleteMyCommands({ language_code: languageCode, scope: this.#scope })
-      ),
-    );
-  }
-}
-
-export class Command<C extends Context> extends Composer<C> {
-  #name: string;
-  #description: string;
-  localizedProperties: Map<string, { name: string; description: string }> = new Map();
+export class Command<C extends Context = Context> extends Composer<C> {
+  #scopes: BotCommandScope[] = [];
+  #languages: Map<string, { name: string; description: string }> = new Map();
 
   constructor(name: string, description: string, ...middleware: Array<Middleware<C>>) {
     super();
@@ -98,51 +28,137 @@ export class Command<C extends Context> extends Composer<C> {
       throw new Error(`${name} is not a valid command name`);
     }
 
-    this.#name = name;
-    this.#description = description;
+    this.#languages.set("default", { name, description });
 
     this.command(name, ...middleware);
   }
 
-  public description(text: string) {
-    this.#description = text;
+  get languages() {
+    return this.#languages;
+  }
+  get scopes() {
+    return this.#scopes;
+  }
+
+  get name() {
+    return this.#languages.get("default")!.name;
+  }
+
+  get description() {
+    return this.#languages.get("default")!.description;
+  }
+
+  public addToScope(type: "default" | "all_private_chats" | "all_group_chats" | "all_chat_administrators"): this;
+  public addToScope(type: "chat", chatId: string | number): this;
+  public addToScope(type: "chat_member", chatId: string | number, userId: number): this;
+  public addToScope(type: "chat_administrators", chatId: string | number): this;
+  public addToScope(type: BotCommandScope["type"], chatId?: string | number, userId?: number): this {
+    switch (type) {
+      case "chat":
+        this.#scopes.push({ type, chat_id: chatId! });
+        break;
+      case "chat_administrators":
+        this.#scopes.push({ type, chat_id: chatId! });
+        break;
+      case "chat_member":
+        this.#scopes.push({ type, chat_id: chatId!, user_id: userId! });
+        break;
+      default:
+        this.#scopes.push({ type });
+        break;
+    }
+
     return this;
   }
 
   public localize(languageCode: string, name: string, description: string) {
-    this.localizedProperties.set(languageCode, { name, description });
+    this.#languages.set(languageCode, { name, description });
     return this;
   }
 
   public getLocalizedName(languageCode: string) {
-    return this.localizedProperties.get(languageCode)?.name ?? this.#name;
+    return this.#languages.get(languageCode)?.name ?? this.name;
   }
 
   public getLocalizedDescription(languageCode: string) {
-    return this.localizedProperties.get(languageCode)?.description ?? this.#description;
+    return this.#languages.get(languageCode)?.description ?? this.description;
   }
 
-  onChatType<T extends Chat["type"]>(
+  public onChatType<T extends Chat["type"]>(
     chatType: MaybeArray<T>,
     ...middleware: Array<CommandMiddleware<ChatTypeContext<C, T>>>
   ) {
-    this.chatType(chatType).command(this.#name, ...middleware);
+    const names = Array.from(this.#languages.values()).map(({ name }) => name);
+    this.chatType(chatType).command(names, ...middleware);
     return this;
   }
 
-  private toLocalizedObject(languageCode: string): BotCommand {
+  public toObject(languageCode = "default"): BotCommand {
     return {
       command: this.getLocalizedName(languageCode),
       description: this.getLocalizedDescription(languageCode),
     };
   }
+}
 
-  public toObject(languageCode?: string): BotCommand {
-    if (languageCode) return this.toLocalizedObject(languageCode);
+export class CommandCollection<C extends Context> {
+  #languages: Set<string> = new Set();
+  #scopes: Map<string, Array<Command<C>>> = new Map();
+  #commands: Command<C>[] = [];
 
-    return {
-      command: this.#name,
-      description: this.#description,
-    };
+  constructor(commands: Command<C>[] = []) {
+    commands.forEach((command) => this.add(command));
+  }
+
+  private addCommandToScope(scope: BotCommandScope, command: Command<C>) {
+    const commands = this.#scopes.get(JSON.stringify(scope)) ?? [];
+    this.#scopes.set(JSON.stringify(scope), commands.concat([command]));
+  }
+
+  public add(...commands: Command<C>[]) {
+    commands.forEach((command) => {
+      this.#commands.push(command);
+      command.scopes.forEach((scope) => this.addCommandToScope(scope, command));
+      Array.from(command.languages.keys()).forEach((language) => this.#languages.add(language));
+    });
+    return this;
+  }
+
+  public toArgs() {
+    const params: SetMyCommandsParams[] = [];
+
+    for (const [scope, commands] of this.#scopes.entries()) {
+      for (const language of this.#languages) {
+        params.push({
+          scope: JSON.parse(scope),
+          language_code: language === "default" ? undefined : language,
+          commands: commands.map((command) => command.toObject(language)) ?? [],
+        });
+      }
+    }
+
+    return params.filter((params) => params.commands.length > 0);
+  }
+
+  public setFor(bot: Bot<C>) {
+    const argsArray = this.toArgs();
+    const promises = argsArray.map((args) => bot.api.raw.setMyCommands(args));
+    return Promise.all(promises);
+  }
+
+  public toJSON() {
+    return this.toArgs();
+  }
+
+  public toString() {
+    return JSON.stringify(this);
+  }
+
+  [Symbol.for("Deno.customInspect")]() {
+    return this.toString();
+  }
+
+  [Symbol.for("nodejs.util.inspect.custom")]() {
+    return this.toString();
   }
 }
--- usage.ts
+++ usage.ts
@@ -1,143 +1,38 @@
-import { Command, CommandScope } from "./src/mod.ts";
+import { Command, CommandCollection } from "./src/mod.ts";
 
 const helpCommand = new Command("help", "Sends help")
-  .localize("de-DE", "hilfe", "Sendet Hilfe");
+  .localize("de-DE", "hilfe", "Sendet Hilfe")
+  .addToScope("default")
+  .addToScope("all_group_chats")
+  .addToScope("all_private_chats")
+  .addToScope("all_chat_administrators")
+  .addToScope("chat_administrators", "@grammyjs")
+  .addToScope("chat", "@LWJerri");
 
-const statsCommand = new Command("stats", "Sends group stats");
+const statsCommand = new Command("stats", "Sends group stats")
+  .addToScope("all_group_chats")
+  .addToScope("all_chat_administrators")
+  .addToScope("chat_administrators", "@grammyjs")
+  .addToScope("chat", "@LWJerri");
+
 const protocolCommand = new Command("protocol", "Sets the protocol")
-  .localize("de-DE", "protokoll", "Legt das Protokoll fest");
+  .localize("de-DE", "protokoll", "Legt das Protokoll fest")
+  .addToScope("all_chat_administrators")
+  .addToScope("chat_administrators", "@grammyjs");
+
 const allStatsCommand = new Command("allstats", "Sends all group stats")
-  .localize("de-DE", "allestats", "Sendet alle Gruppenstatistiken");
-const trollCommand = new Command("troll", "Trolls...?");
+  .localize("de-DE", "allestats", "Sendet alle Gruppenstatistiken")
+  .addToScope("chat_administrators", "@grammyjs");
 
-const defaultScope = CommandScope.default().add(helpCommand);
-const groupScope = CommandScope.groups().add([helpCommand, statsCommand]);
-const adminsScope = CommandScope.admins().add([helpCommand, statsCommand, protocolCommand]);
-const grammyMembersScope = CommandScope.chat("@grammyjs").add([helpCommand, statsCommand]);
-const grammyAdminsScope = CommandScope.chat("@grammyjs").add([helpCommand, statsCommand, allStatsCommand]);
-const lwjerryScope = CommandScope.chat("@lwjerry").add([helpCommand, trollCommand]);
+const trollCommand = new Command("troll", "Trolls...?")
+  .addToScope("chat", "@LWJerri");
 
-const scopes = [
-  defaultScope,
-  groupScope,
-  adminsScope,
-  grammyMembersScope,
-  grammyAdminsScope,
-  lwjerryScope,
-];
+const commands = new CommandCollection([
+  helpCommand,
+  statsCommand,
+  protocolCommand,
+  allStatsCommand,
+  trollCommand,
+]);
 
-for (const scope of scopes) {
-  console.log(scope.toParams());
-}
-
-/**
- * [
- *   {
- *     scope: { type: "default" },
- *     language_code: "de-DE",
- *     commands: [ { command: "hilfe", description: "Sendet Hilfe" } ]
- *   },
- *   {
- *     scope: { type: "default" },
- *     language_code: undefined,
- *     commands: [ { command: "help", description: "Sends help" } ]
- *   }
- * ]
- * [
- *   {
- *     scope: { type: "all_group_chats" },
- *     language_code: "de-DE",
- *     commands: [
- *       { command: "hilfe", description: "Sendet Hilfe" },
- *       { command: "stats", description: "Sends group stats" }
- *     ]
- *   },
- *   {
- *     scope: { type: "all_group_chats" },
- *     language_code: undefined,
- *     commands: [
- *       { command: "help", description: "Sends help" },
- *       { command: "stats", description: "Sends group stats" }
- *     ]
- *   }
- * ]
- * [
- *   {
- *     scope: { type: "all_chat_administrators" },
- *     language_code: "de-DE",
- *     commands: [
- *       { command: "hilfe", description: "Sendet Hilfe" },
- *       { command: "stats", description: "Sends group stats" },
- *       { command: "protokoll", description: "Legt das Protokoll fest" }
- *     ]
- *   },
- *   {
- *     scope: { type: "all_chat_administrators" },
- *     language_code: undefined,
- *     commands: [
- *       { command: "help", description: "Sends help" },
- *       { command: "stats", description: "Sends group stats" },
- *       { command: "protocol", description: "Sets the protocol" }
- *     ]
- *   }
- * ]
- * [
- *   {
- *     scope: { type: "chat", chat_id: "@grammyjs" },
- *     language_code: "de-DE",
- *     commands: [
- *       { command: "hilfe", description: "Sendet Hilfe" },
- *       { command: "stats", description: "Sends group stats" }
- *     ]
- *   },
- *   {
- *     scope: { type: "chat", chat_id: "@grammyjs" },
- *     language_code: undefined,
- *     commands: [
- *       { command: "help", description: "Sends help" },
- *       { command: "stats", description: "Sends group stats" }
- *     ]
- *   }
- * ]
- * [
- *   {
- *     scope: { type: "chat", chat_id: "@grammyjs" },
- *     language_code: "de-DE",
- *     commands: [
- *       { command: "hilfe", description: "Sendet Hilfe" },
- *       { command: "stats", description: "Sends group stats" },
- *       {
- *         command: "allestats",
- *         description: "Sendet alle Gruppenstatistiken"
- *       }
- *     ]
- *   },
- *   {
- *     scope: { type: "chat", chat_id: "@grammyjs" },
- *     language_code: undefined,
- *     commands: [
- *       { command: "help", description: "Sends help" },
- *       { command: "stats", description: "Sends group stats" },
- *       { command: "allstats", description: "Sends all group stats" }
- *     ]
- *   }
- * ]
- * [
- *   {
- *     scope: { type: "chat", chat_id: "@lwjerry" },
- *     language_code: "de-DE",
- *     commands: [
- *       { command: "hilfe", description: "Sendet Hilfe" },
- *       { command: "troll", description: "Trolls...?" }
- *     ]
- *   },
- *   {
- *     scope: { type: "chat", chat_id: "@lwjerry" },
- *     language_code: undefined,
- *     commands: [
- *       { command: "help", description: "Sends help" },
- *       { command: "troll", description: "Trolls...?" }
- *     ]
- *   }
- * ]
- */
+Deno.writeTextFile("./scopes.json", JSON.stringify(commands, null, 2));

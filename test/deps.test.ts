b07f7eab932d502f4d5e4bc8b84828335e174e73
Rogerio Munhoz <roz@rjmunhoz.me>
2023-10-06 11:35:54 -0300

    test: add a few tests

---
+++ test/deps.test.ts
@@ -0,0 +1,15 @@
+export {
+  assert,
+  assertEquals,
+  assertFalse,
+  assertInstanceOf,
+  assertNotStrictEquals,
+  assertObjectMatch,
+  assertRejects,
+  assertStringIncludes,
+  assertThrows,
+} from "https://deno.land/std@0.203.0/assert/mod.ts";
+export { afterEach, beforeEach, describe, it } from "https://deno.land/std@0.203.0/testing/bdd.ts";
+export { type Spy, spy, type Stub, stub } from "https://deno.land/std@0.203.0/testing/mock.ts";
+export { Api, Context } from "https://lib.deno.dev/x/grammy@1/mod.ts";
+export type { Chat, Message, Update, User, UserFromGetMe } from "https://lib.deno.dev/x/grammy@1/types.ts";

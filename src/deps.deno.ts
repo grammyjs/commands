7631c1fa6be9734dc739fa54a46bb28c70468295
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-27 19:13:04 -0400

    reorganize line position

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -3,13 +3,13 @@ export {
     Bot,
     type ChatTypeContext,
     type ChatTypeMiddleware,
+    type CommandContext,
     type CommandMiddleware,
     Composer,
     Context,
     type Middleware,
     type MiddlewareObj,
     type NextFunction,
-    type CommandContext,
 } from "https://lib.deno.dev/x/grammy@1/mod.ts";
 export type {
     BotCommand,

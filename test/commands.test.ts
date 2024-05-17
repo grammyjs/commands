1ff6856e6aebd472560233dd9be3b75a4ab46d16
Hero Protagonist <heroprotagonist32@gmail.com>
2024-05-17 15:14:35 -0400

    add test's for setMyCommands utils functions and fuzzyMatch regex commands

--- test/commands.test.ts
+++ test/commands.test.ts
@@ -43,4 +43,92 @@ describe("Commands", () => {
             );
         });
     });
+    describe("setMyCommands utils", () => {
+        describe("toSingleScopeArgs", () => {
+            it("should omit regex commands", () => {
+                const commands = new Commands();
+                commands.command("test", "handler1", (_) => _);
+                commands.command("test2", "handler2", (_) => _);
+                commands.command(/omitMe_\d\d/, "handler3", (_) => _);
+                const params = commands.toSingleScopeArgs({
+                    type: "chat",
+                    chat_id: 10,
+                });
+                assertEquals(params, [
+                    {
+                        scope: { type: "chat", chat_id: 10 },
+                        language_code: undefined,
+                        commands: [
+                            { command: "test", description: "handler1" },
+                            { command: "test2", description: "handler2" },
+                        ],
+                    },
+                ]);
+            });
+            it("should return an array with the localized versions of commands", () => {
+                const commands = new Commands();
+                commands.command("test", "handler1", (_) => _).localize(
+                    "es",
+                    "prueba1",
+                    "resolvedor1",
+                );
+                commands.command("test2", "handler2", (_) => _);
+                commands.command(/omitMe_\d\d/, "handler3", (_) => _).localize(
+                    "es",
+                    /omiteme_\d/,
+                    "resolvedor3",
+                );
+
+                const params = commands.toSingleScopeArgs({
+                    type: "chat",
+                    chat_id: 10,
+                });
+                assertEquals(params, [
+                    {
+                        scope: { type: "chat", chat_id: 10 },
+                        language_code: undefined,
+                        commands: [
+                            { command: "test", description: "handler1" },
+                            { command: "test2", description: "handler2" },
+                        ],
+                    },
+                    {
+                        scope: {
+                            chat_id: 10,
+                            type: "chat",
+                        },
+                        language_code: "es",
+                        commands: [
+                            {
+                                command: "prueba1",
+                                description: "resolvedor1",
+                            },
+                            {
+                                command: "test2",
+                                description: "handler2",
+                            },
+                        ],
+                    },
+                ]);
+            });
+            it("should omit commands with no handler", () => {
+                const commands = new Commands();
+                commands.command("test", "handler", (_) => _);
+                commands.command("omitme", "nohandler");
+                const params = commands.toSingleScopeArgs({
+                    type: "chat",
+                    chat_id: 10,
+                });
+                assertEquals(params, [
+                    {
+                        scope: { type: "chat", chat_id: 10 },
+                        language_code: undefined,
+                        commands: [
+                            { command: "test", description: "handler" },
+                        ],
+                    },
+                ]);
+            });
+        });
+    });
 });

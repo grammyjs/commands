fc0f06bc3f9be212a066d9fd71b99b0487ca5fc4
Hero Protagonist <heroprotagonist32@gmail.com>
2024-06-04 21:20:11 -0400

    export dummy ctx function

--- test/context.test.ts
+++ test/context.test.ts
@@ -13,53 +13,43 @@ import {
 } from "./deps.test.ts";
 
 describe("commands", () => {
-    const u = { id: 42, first_name: "bot", is_bot: true } as User;
-    const c = { id: 100, type: "private" } as Chat;
-    const m = { text: "a", from: u, chat: c, sender_chat: c } as Message;
-    const update = {
-        message: m,
-        edited_message: m,
-        channel_post: m,
-        edited_channel_post: m,
-        inline_query: { id: "b", from: u, query: "iq" },
-        chosen_inline_result: {
-            from: u,
-            inline_message_id: "x",
-            result_id: "p",
-        },
-        callback_query: {
-            data: "cb",
-            game_short_name: "game",
-            message: m,
-            from: u,
-            inline_message_id: "y",
-        },
-        shipping_query: { id: "d", from: u },
-        pre_checkout_query: { id: "e", from: u },
-        poll: { id: "f" },
-        poll_answer: { poll_id: "g" },
-        my_chat_member: { date: 1, from: u, chat: c },
-        chat_member: { date: 2, from: u, chat: c },
-        chat_join_request: { date: 3, from: u, chat: c },
-    } as Update;
-
-    const api = new Api("dummy-token");
-    const me = { id: 42, username: "bot" } as UserFromGetMe;
 
     it("should install the setMyCommands method on the context", () => {
-        const context = new Context(update, api, me) as CommandsFlavor<Context>;
+        const context = dummyCtx({})
 
         const middleware = commands();
-        middleware(context, async () => {});
+        middleware(context, async () => { });
 
         assert(context.setMyCommands);
     });
     it("should install the getNearestCommand method on the context", () => {
-        const context = new Context(update, api, me) as CommandsFlavor<Context>;
+        const context = dummyCtx({})
 
         const middleware = commands();
-        middleware(context, async () => {});
+        middleware(context, async () => { });
 
         assert(context.getNearestCommand);
     });
 });
+
+
+export function dummyCtx({ userInput, language, noChat }: {
+    userInput?: string, language?: string, noChat?: boolean
+}) {
+    const u = { id: 42, first_name: "yo", language_code: language } as User;
+    const c = { id: 100, type: "private" } as Chat;
+    const m = {
+        text: userInput,
+        from: u,
+        chat: noChat ? undefined : c,
+    } as Message;
+    const update = {
+        message: m,
+    } as Update;
+    const api = new Api("dummy-token");
+    const me = { id: 42, username: "bot" } as UserFromGetMe;
+    const ctx = new Context(update, api, me) as CommandsFlavor<Context>;
+    const middleware = commands();
+    middleware(ctx, async () => { });
+    return ctx;
+}

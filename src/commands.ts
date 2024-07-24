e959f5ca5ed18c48891f85b356d6c3052ad0ab9c
Hero Protagonist <heroprotagonist32@gmail.com>
2024-07-24 11:22:39 -0400

    Revert "add getter to private _commands field"
    This reverts commit 4e4938fe48bde6fd20b39d9f010e4459f4e9e5ef.

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,4 @@
-import { Command } from './command.ts'
+import { Command } from "./command.ts";
 import {
     Api,
     BotCommand,
@@ -8,12 +8,9 @@ import {
     Context,
     type LanguageCode,
     Middleware,
-} from './deps.deno.ts'
-import type {
-    CommandElementals,
-    CommandOptions,
-} from './types.ts'
-import { type MaybeArray } from './utils.ts'
+} from "./deps.deno.ts";
+import type { CommandElementals, CommandOptions } from "./types.ts";
+import { type MaybeArray } from "./utils.ts";
 
 /**
  * Interface for grouping {@link BotCommand}s that might (or not)
@@ -21,31 +18,31 @@ import { type MaybeArray } from './utils.ts'
  */
 export type SetMyCommandsParams = {
     /** If defined: scope on which the commands will take effect */
-    scope?: BotCommandScope
+    scope?: BotCommandScope;
     /** If defined: Language on which the commands will take effect.
      * Two letter abbreviation in ISO_639 standard: https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
      */
-    language_code?: LanguageCode
+    language_code?: LanguageCode;
     /** Commands that can be each one passed to a SetMyCommands Call */
-    commands: BotCommand[]
-}
+    commands: BotCommand[];
+};
 
 const isMiddleware = <C extends Context>(
-    obj: unknown
+    obj: unknown,
 ): obj is MaybeArray<Middleware<C>> => {
-    if (!obj) return false
-    if (Array.isArray(obj)) return obj.every(isMiddleware)
-    const objType = typeof obj
+    if (!obj) return false;
+    if (Array.isArray(obj)) return obj.every(isMiddleware);
+    const objType = typeof obj;
 
     switch (objType) {
-        case 'function':
-            return true
-        case 'object':
-            return Object.keys(obj).includes('middleware')
+        case "function":
+            return true;
+        case "object":
+            return Object.keys(obj).includes("middleware");
     }
 
-    return false
-}
+    return false;
+};
 
 /**
  * Central class that manages all registered commands.
@@ -61,46 +58,37 @@ const isMiddleware = <C extends Context>(
  * ```
  */
 export class Commands<C extends Context> {
-    private _languages: Set<LanguageCode | 'default'> =
-        new Set()
-    private _scopes: Map<string, Array<Command<C>>> =
-        new Map()
-    private _commands: Command<C>[] = []
+    private _languages: Set<LanguageCode | "default"> = new Set();
+    private _scopes: Map<string, Array<Command<C>>> = new Map();
+    private _commands: Command<C>[] = [];
 
-    private _cachedComposer: Composer<C> = new Composer()
-    private _cachedComposerInvalidated: boolean = false
+    private _cachedComposer: Composer<C> = new Composer();
+    private _cachedComposerInvalidated: boolean = false;
 
-    private _commandOptions: Partial<CommandOptions> = {}
+    private _commandOptions: Partial<CommandOptions> = {};
 
     constructor(options: Partial<CommandOptions> = {}) {
-        this._commandOptions = options
+        this._commandOptions = options;
     }
 
-    private _addCommandToScope(
-        scope: BotCommandScope,
-        command: Command<C>
-    ) {
-        const commands =
-            this._scopes.get(JSON.stringify(scope)) ?? []
-        this._scopes.set(
-            JSON.stringify(scope),
-            commands.concat([command])
-        )
+    private _addCommandToScope(scope: BotCommandScope, command: Command<C>) {
+        const commands = this._scopes.get(JSON.stringify(scope)) ?? [];
+        this._scopes.set(JSON.stringify(scope), commands.concat([command]));
     }
 
     private _populateMetadata() {
-        this._languages.clear()
-        this._scopes.clear()
+        this._languages.clear();
+        this._scopes.clear();
 
         this._commands.forEach((command) => {
             for (const scope of command.scopes) {
-                this._addCommandToScope(scope, command)
+                this._addCommandToScope(scope, command);
             }
 
             for (const language of command.languages.keys()) {
-                this._languages.add(language)
+                this._languages.add(language);
             }
-        })
+        });
     }
 
     /**
@@ -115,8 +103,8 @@ export class Commands<C extends Context> {
         name: string | RegExp,
         description: string,
         handler: MaybeArray<Middleware<CommandContext<C>>>,
-        options?: Partial<CommandOptions>
-    ): Command<C>
+        options?: Partial<CommandOptions>,
+    ): Command<C>;
     /**
      * Registers a new command with no handlers.
      * @param name Default command name
@@ -127,71 +115,54 @@ export class Commands<C extends Context> {
     public command(
         name: string | RegExp,
         description: string,
-        options?: Partial<CommandOptions>
-    ): Command<C>
+        options?: Partial<CommandOptions>,
+    ): Command<C>;
     public command(
         name: string | RegExp,
         description: string,
         handlerOrOptions?:
             | MaybeArray<Middleware<CommandContext<C>>>
             | Partial<CommandOptions>,
-        _options?: Partial<CommandOptions>
+        _options?: Partial<CommandOptions>,
     ) {
         const handler = isMiddleware(handlerOrOptions)
             ? handlerOrOptions
-            : undefined
+            : undefined;
         const options = handler
             ? _options ?? this._commandOptions
-            : (handlerOrOptions as Partial<CommandOptions>) ??
-              this._commandOptions
+            : handlerOrOptions as Partial<CommandOptions> ??
+                this._commandOptions;
 
-        const command = new Command<C>(
-            name,
-            description,
-            options
-        )
-        if (handler)
-            command.addToScope({ type: 'default' }, handler)
+        const command = new Command<C>(name, description, options);
+        if (handler) command.addToScope({ type: "default" }, handler);
 
-        this._commands.push(command)
-        this._cachedComposerInvalidated = true
-        return command
+        this._commands.push(command);
+        this._cachedComposerInvalidated = true;
+        return command;
     }
     /**
      * Serializes the commands into multiple objects that can each be passed to a `setMyCommands` call.
      * @returns One item for each combination of command + scope + language
      */
     public toArgs() {
-        this._populateMetadata()
-        const params: SetMyCommandsParams[] = []
+        this._populateMetadata();
+        const params: SetMyCommandsParams[] = [];
 
-        for (const [
-            scope,
-            commands,
-        ] of this._scopes.entries()) {
+        for (const [scope, commands] of this._scopes.entries()) {
             for (const language of this._languages) {
                 params.push({
                     scope: JSON.parse(scope),
-                    language_code:
-                        language === 'default'
-                            ? undefined
-                            : language,
+                    language_code: language === "default"
+                        ? undefined
+                        : language,
                     commands: commands
-                        .filter(
-                            (command) =>
-                                typeof command.name ===
-                                'string'
-                        )
-                        .map((command) =>
-                            command.toObject(language)
-                        ),
-                })
+                        .filter((command) => typeof command.name === "string")
+                        .map((command) => command.toObject(language)),
+                });
             }
         }
 
-        return params.filter(
-            (params) => params.commands.length > 0
-        )
+        return params.filter((params) => params.commands.length > 0);
     }
 
     /**
@@ -201,29 +172,19 @@ export class Commands<C extends Context> {
      * @returns One item per command per language
      */
     public toSingleScopeArgs(scope: BotCommandScope) {
-        this._populateMetadata()
-        const params: SetMyCommandsParams[] = []
+        this._populateMetadata();
+        const params: SetMyCommandsParams[] = [];
         for (const language of this._languages) {
             params.push({
                 scope,
-                language_code:
-                    language === 'default'
-                        ? undefined
-                        : language,
+                language_code: language === "default" ? undefined : language,
                 commands: this._commands
-                    .filter(
-                        (command) => command.scopes.length
-                    )
-                    .filter(
-                        (command) =>
-                            typeof command.name === 'string'
-                    )
-                    .map((command) =>
-                        command.toObject(language)
-                    ),
-            })
+                    .filter((command) => command.scopes.length)
+                    .filter((command) => typeof command.name === "string")
+                    .map((command) => command.toObject(language)),
+            });
         }
-        return params
+        return params;
     }
 
     /**
@@ -239,10 +200,8 @@ export class Commands<C extends Context> {
      */
     public async setCommands({ api }: { api: Api }) {
         await Promise.all(
-            this.toArgs().map((args) =>
-                api.raw.setMyCommands(args)
-            )
-        )
+            this.toArgs().map((args) => api.raw.setMyCommands(args)),
+        );
     }
 
     /**
@@ -258,71 +217,56 @@ export class Commands<C extends Context> {
      */
 
     public toElementals(
-        filterLanguage?: LanguageCode | 'default'
+        filterLanguage?: LanguageCode | "default",
     ): CommandElementals[] {
-        this._populateMetadata()
+        this._populateMetadata();
 
         return Array.from(this._scopes.values())
             .flat()
-            .flatMap((command) => {
-                const elements = []
-                for (const [
-                    language,
-                    local,
-                ] of command.languages.entries()) {
-                    elements.push({
-                        name:
-                            local.name instanceof RegExp
+            .flatMap(
+                (command) => {
+                    const elements = [];
+                    for (
+                        const [language, local] of command.languages.entries()
+                    ) {
+                        elements.push({
+                            name: local.name instanceof RegExp
                                 ? local.name.source
                                 : local.name,
-                        language,
-                        prefix: command.prefix,
-                        scopes: command.scopes,
-                        description:
-                            command.getLocalizedDescription(
-                                language
+                            language,
+                            prefix: command.prefix,
+                            scopes: command.scopes,
+                            description: command.getLocalizedDescription(
+                                language,
                             ),
-                    })
-                }
-                if (filterLanguage) {
-                    const filtered = elements.filter(
-                        (command) =>
-                            command.language ===
-                            filterLanguage
-                    )
-                    const defaulted = elements.filter(
-                        (command) =>
-                            command.language === 'default'
-                    )
-                    return filtered.length
-                        ? filtered[0]
-                        : defaulted[0]
-                } else return elements
-            })
+                        });
+                    }
+                    if (filterLanguage) {
+                        const filtered = elements.filter((command) =>
+                            command.language === filterLanguage
+                        );
+                        const defaulted = elements.filter((command) =>
+                            command.language === "default"
+                        );
+                        return filtered.length ? filtered[0] : defaulted[0];
+                    } else return elements;
+                },
+            );
     }
 
     /**
      * @returns A JSON serialized version of all the currently registered commands
      */
     public toString() {
-        return JSON.stringify(this)
+        return JSON.stringify(this);
     }
 
     middleware() {
         if (this._cachedComposerInvalidated) {
-            this._cachedComposer = new Composer(
-                ...this._commands
-            )
-            this._cachedComposerInvalidated = false
+            this._cachedComposer = new Composer(...this._commands);
+            this._cachedComposerInvalidated = false;
         }
-        return this._cachedComposer.middleware()
-    }
-
-    /**
-     * @returns all commands contained in the instance
-     */
-    public get commands(): Command<C>[] {
-        return this._commands
+        return this._cachedComposer.middleware();
     }
 
     /**
@@ -330,8 +274,8 @@ export class Commands<C extends Context> {
      *
      * @see toString
      */
-    [Symbol.for('Deno.customInspect')]() {
-        return this.toString()
+    [Symbol.for("Deno.customInspect")]() {
+        return this.toString();
     }
 
     /**
@@ -339,7 +283,7 @@ export class Commands<C extends Context> {
      *
      * @see toString
      */
-    [Symbol.for('nodejs.util.inspect.custom')]() {
-        return this.toString()
+    [Symbol.for("nodejs.util.inspect.custom")]() {
+        return this.toString();
     }
 }

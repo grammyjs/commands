6c0411a3afa69dfc7473c1e14a43bbc8f2ac31ef
Hero Protagonist <heroprotagonist32@gmail.com>
2025-02-12 14:01:52 -0300

    fix: add missing LanguageCodes export

--- src/mod.ts
+++ src/mod.ts
@@ -2,3 +2,4 @@ export { Command } from "./command.ts";
 export * from "./command-group.ts";
 export * from "./context.ts";
 export type { CommandOptions } from "./types.ts";
+export { LanguageCodes } from "./language-codes.ts";
